00b4c34db2dbff5f61d8471329550836e085ac19
Volodymyr Horshenin <volodymyr.horshenin@gmail.com>
2026-03-22 19:34:39 +0100

    refactor: ♻️ use colorsys for color conversions

--- src/pyansistring/_helpers.py
+++ src/pyansistring/_helpers.py
@@ -25,6 +25,7 @@ __all__ = [
 
 import math
 from collections.abc import Generator
+from colorsys import hls_to_rgb
 from dataclasses import dataclass
 from pathlib import Path
 from string import Formatter as _Formatter
@@ -127,16 +128,8 @@ def hsl_to_rgb(
     hue: int | float, saturation: int | float = 100, lightness: int | float = 50
 ) -> tuple[int, int, int]:
     """Convert HSL color values to RGB."""
-    hue = hue / 100
-    saturation = saturation / 100
-    lightness = lightness / 100
-
-    def f(n: int | float):
-        k = (n + hue * (10 / 3)) % 12
-        a = saturation * min(lightness, 1 - lightness)
-        return round((lightness - a * max(-1, min(k - 3, 9 - k, 1))) * 255)
-
-    return f(0), f(8), f(4)
+    r, g, b = hls_to_rgb(hue / 360, lightness / 100, saturation / 100)
+    return int(round(r * 255)), int(round(g * 255)), int(round(b * 255))
 
 
 def load_font(font: "TTFont | Path | str") -> "TTFont":
