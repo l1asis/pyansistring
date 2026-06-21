178d75b58d0d532587656d7c104eba545138807a
Volodymyr Horshenin <volodymyr.horshenin@gmail.com>
2026-06-21 21:25:09 +0200

    feat: ✨ introduce new types for intuitive inputs

--- src/pyansistring/_types.py
+++ src/pyansistring/_types.py
@@ -1,6 +1,10 @@
 from __future__ import annotations
 
 __all__ = [
+    "FgInput",
+    "BgInput",
+    "UlInput",
+    "ColorInput",
     "ColorStop",
     "Coordinate",
     "CoordinateGroup",
@@ -13,7 +17,23 @@ from typing import (
 )
 
 from .color import Color as _Color
+from .constants import (
+    Background as _Background,
+    Foreground as _Foreground,
+    Palette8Bit as _Palette8Bit,
+    Underline as _Underline,
+)
 
+ColorInput: _TypeAlias = _Color | tuple[int | int | int] | str | _Palette8Bit | int
+FgInput: _TypeAlias = (
+    _Foreground | _Palette8Bit | int | tuple[int, int, int] | str | _Color
+)
+BgInput: _TypeAlias = (
+    _Background | _Palette8Bit | int | tuple[int, int, int] | str | _Color
+)
+UlInput: _TypeAlias = (
+    _Underline | _Palette8Bit | int | tuple[int, int, int] | str | _Color
+)
 ColorStop: _TypeAlias = _Color | tuple[int, int, int]
 Coordinate: _TypeAlias = tuple[int, int]
 CoordinateGroup: _TypeAlias = Coordinate | tuple[Coordinate, ...]
